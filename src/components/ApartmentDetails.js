013edd56c485f7458f2d8da9a7a707f04e5cedbc
Luluyan2022 <emmymagao@gmail.com>
2023-01-20 21:03:32 +0100

    done

--- src/components/ApartmentDetails.js
+++ src/components/ApartmentDetails.js
@@ -1,7 +1,30 @@
-export default function ApartmentDetails(){
+import { Link, useParams } from "react-router-dom"
+
+export default function ApartmentDetails(props){
+    const {apartmentId} = useParams();
+    console.log(props.apartmentsList)
+    const datails = props.apartmentsList?.find(apartmentDetails => {
+        console.log(apartmentDetails)
+        // eslint-disable-next-line
+        return apartmentDetails._id == apartmentId;
+    });
+    console.log(datails)
+
+    const renderDetails = () => {
+        return(
+            <div>
+                <h1>{datails.title} </h1>
+                <img src={datails.img} alt={datails.name} /><br />                 
+                pricePerDay: {datails.pricePerDay} <br />
+                
+            </div>
+        )
+    }
     return(
         <div>
-            ?
+            { datails && renderDetails() }
+
+            <Link to="/apartments">Back</Link>
         </div>
     )
 }
